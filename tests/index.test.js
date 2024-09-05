b70578447960cd8a5d9b7bacbf154679ab12d2d6
SamaraPalladino <luizamaradelg@gmail.com>
2024-09-05 10:50:01 -0300

    inserção pasta workflow

---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+const { soma, multiplicacao } = require('../src/index');
+
+// Testes existentes para soma...
+test('soma 1 + 2 para igualar 3', () => {
+  expect(soma(1, 2)).toBe(3);
+});
+
+// Novos testes para multiplicacao
+test('multiplicacao 3 * 4 para igualar 12', () => {
+  expect(multiplicacao(3, 4)).toBe(12);
+});
